8e7669b1cb3fa17aff0b0d213d090346094b42ee
Mouhsm <37739920+Mouhsm@users.noreply.github.com>
2024-08-29 12:38:45 +0100

    Update ai-blog-writer.py

--- ai-blog-writer.py
+++ ai-blog-writer.py
@@ -2,6 +2,15 @@ import streamlit as st
 import requests
 import os
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # Get the API key from the environment variable
 API_KEY = os.getenv('GEMINI_API_KEY')
 
